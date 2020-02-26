f9b6658f5171acd2e1a0511573e3130bdeef6be5
KharchenkoAndrey <54741921+KharchenkoAndrey@users.noreply.github.com>
2020-02-26 21:08:53 +0700

    Добавляет функцию связи полей комнат и гостей

--- js/main.js
+++ js/main.js
@@ -117,30 +117,34 @@ formHeader.setAttribute('disabled', 'disabled');
 formFeatures.setAttribute('disabled', 'disabled');
 
 var rooms = document.querySelector('#room_number');
-var guests = document.querySelector('#capacity').options;
+var guests = document.querySelector('#capacity');
 
-rooms.addEventListener('change', function (evt) {
-  var target = evt.target.value;
 
-  if (target === '1') {
-    guests[2].selected = true;
-    guests[0].setAttribute('disabled', 'disabled');
-    guests[1].setAttribute('disabled', 'disabled');
-    guests[3].setAttribute('disabled', 'disabled');
-  }
-  if (target === '2') {
-    guests[1].selected = true;
-    guests[0].setAttribute('disabled', 'disabled');
-    guests[3].setAttribute('disabled', 'disabled');
-  }
-  if (target === '3') {
-    guests[0].selected = true;
-    guests[3].setAttribute('disabled', 'disabled');
-  }
-  if (target === '100') {
-    guests[3].selected = true;
-    guests[2].setAttribute('disabled', 'disabled');
-    guests[1].setAttribute('disabled', 'disabled');
-    guests[0].setAttribute('disabled', 'disabled');
-  }
-});
+function roomsGuests(select1, select2) {
+  var optionMap = {
+    1: [1],
+    2: [1, 2],
+    3: [3, 2, 1],
+    100: [0]
+  };
+
+  return function () {
+    var value = +select1.value;
+    var options = select2.options;
+    var optionsLength = options.length;
+    var availableOptions = optionMap[value];
+
+    for (var i = 0; i < optionsLength; i++) {
+      if (availableOptions.indexOf(+options[i].value) !== -1) {
+        options[i].disabled = false;
+        if (+options[i].value === value || availableOptions.length === 1) {
+          options[i].selected = true;
+        }
+      } else {
+        options[i].disabled = true;
+      }
+    }
+  };
+}
+
+rooms.addEventListener('change', roomsGuests(rooms, guests));
